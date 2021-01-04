de1cc91c258d0f2c3971a656cad11d27214bfdf5
Vishvak Seenichamy <vishvak365@gmail.com>
2021-01-04 11:21:12 -0500

    Add account features and tests

--- Routes/Accounts/Account_Creation.js
+++ Routes/Accounts/Account_Creation.js
@@ -2,91 +2,60 @@ var express = require("express");
 var router = express.Router();
 const account = require("../models/account");
 const firebase = require("../firebase/config");
-router.get("/fb/create_account", function (req, res) {
-  firebase
-    .auth()
-    .createUser({
-      email: "vishvak@vishvak.com",
-      password: "111111123",
-    })
-    .then((userRecord) => {
-      // See the UserRecord reference doc for the contents of userRecord.
-      console.log("Successfully created new user:", userRecord.uid);
-    })
-    .catch((error) => {
-      console.log("Error creating new user:", error);
-    });
-  res.send();
+router.post("/fb/create_account", function (req, res) {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).send({ message: "Email or Password not provided" });
+  } else {
+    firebase
+      .auth()
+      .createUser({
+        email: req.body.email,
+        password: req.body.password,
+      })
+      .then((userRecord) => {
+        console.log("Successfully created new user:", userRecord.uid);
+        res.status(200).json(userRecord);
+      })
+      .catch((error) => {
+        res.status(400).json(error.errorInfo).send();
+      });
+  }
 });
-router.get("/fb/login", function (req, res) {
-  const headerToken = request.headers.authorization;
+router.post("/fb/remove_account", function (req, res) {
+  if (!req.body.uid) {
+    res.status(400).send({ message: "UID not provided" });
+  } else {
+    firebase
+      .auth()
+      .deleteUser(req.body.uid)
+      .then((message) => {
+        res.status(200).json({ message: "Successfully removed user" });
+      })
+      .catch((error) => {
+        res.status(400).json(error.errorInfo).send();
+      });
+  }
+});
+router.post("/fb/login", function (req, res) {
+  const headerToken = req.headers.authorization;
   if (!headerToken) {
-    return response.send({ message: "No token provided" }).status(401);
+    return res.send({ message: "No token provided" }).status(401);
   }
 
   if (headerToken && headerToken.split(" ")[0] !== "Bearer") {
-    response.send({ message: "Invalid token" }).status(401);
+    res.send({ message: "Invalid token" }).status(401);
   }
 
   const token = headerToken.split(" ")[1];
   firebase
     .auth()
     .verifyIdToken(token)
-    .then(() => next())
-    .catch(() => response.send({ message: "Could not authorize" }).status(403));
-});
-router.post("/create_account", function (req, res) {
-  req_body = req.body;
-  if (
-    !req_body.username ||
-    !req_body.email ||
-    !req_body.first_name ||
-    !req_body.last_name ||
-    !req_body.phone_number ||
-    !req_body.password
-  ) {
-    res.status(400).send({ error: "Insufficient information" });
-  } else {
-    let new_account = new account({
-      username: req.body.username,
-      email: req.body.email,
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      phone_number: req.body.phone_number,
-      password: req.body.password,
+    .then(() => {
+      /* istanbul ignore next */
+      next();
+    })
+    .catch((error) => {
+      res.status(403).send(error.errorInfo);
     });
-    new_account.save(function (err, account) {
-      if (err) {
-        /* istanbul ignore next */
-        if (err.message.includes("E11000")) {
-          res.status(409).send({
-            error: "Email, Phone, or Username is already Taken",
-          });
-        }
-      } else {
-        res.status(200).send({
-          success: "Account Created Successfully",
-        });
-      }
-    });
-  }
-});
-router.post("/remove_account", function (req, res) {
-  if (!req.body.username) {
-    res.status(400).send({ error: "Insufficient information" });
-  } else {
-    account.findOneAndDelete(
-      { username: req.body.username },
-      function (err, account) {
-        /* istanbul ignore next */
-        if (err) {
-          /* istanbul ignore next */
-          res.send(err);
-        } else {
-          res.status(200).send({ success: "Successfully removed user" });
-        }
-      }
-    );
-  }
 });
 module.exports = router;
--- tests/accounts.test.js
+++ tests/accounts.test.js
@@ -1,7 +1,7 @@
 const app = require("../index");
 const supertest = require("supertest");
 const request = supertest(app);
-describe("Base Endpoints", () => {
+describe("Account Creation,Deletion,Validation", () => {
   let server = null;
 
   beforeEach(() => {
@@ -11,63 +11,108 @@ describe("Base Endpoints", () => {
   afterEach(async () => {
     // await server.close();
   });
+  let newUID;
+  it("Tries to create new account", async (done) => {
+    const structure = { emailVerified: false, disabled: false };
+    const response = await request.post("/api/account/fb/create_account").send({
+      email: "testEmail@test.com",
+      password: "3kljd*ksld",
+    });
+    newUID = response.body.uid;
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject(structure);
+    done();
+  });
   it("Tries to remove existing account", async (done) => {
-    const response = await request.post("/api/account/remove_account").send({
-      username: "duplicate",
+    const response = await request.post("/api/account/fb/remove_account").send({
+      uid: newUID,
     });
     expect(response.status).toBe(200);
     expect(response.body).toStrictEqual({
-      success: "Successfully removed user",
+      message: "Successfully removed user",
     });
     done();
   });
-  it("Tries to remove existing account with unsufficient information", async (done) => {
-    const response = await request.post("/api/account/remove_account").send({
-      last_name: "duplicate",
+  it("Tries to create account with insufficient information", async (done) => {
+    const response = await request.post("/api/account/fb/create_account").send({
+      nonValid: "nonValid",
     });
     expect(response.status).toBe(400);
-    expect(response.body).toStrictEqual({ error: "Insufficient information" });
-    done();
-  });
-  it("Tries to add new account", async (done) => {
-    const response = await request.post("/api/account/create_account").send({
-      username: "duplicate",
-      email: "duplicate",
-      first_name: "duplicate",
-      last_name: "duplicate",
-      phone_number: "duplicate",
-      password: "duplicate",
-    });
-    expect(response.status).toBe(200);
     expect(response.body).toStrictEqual({
-      success: "Account Created Successfully",
+      message: "Email or Password not provided",
     });
     done();
   });
-  it("Tries to add existing account", async (done) => {
-    const response = await request.post("/api/account/create_account").send({
-      username: "duplicate",
-      email: "duplicate",
-      first_name: "duplicate",
-      last_name: "duplicate",
-      phone_number: "duplicate",
-      password: "duplicate",
-    });
-    expect(response.status).toBe(409);
-    expect(response.body).toStrictEqual({
-      error: "Email, Phone, or Username is already Taken",
-    });
-    done();
-  });
-  it("Tries to submit insufficient account creation information", async (done) => {
-    const response = await request.post("/api/account/create_account").send({
-      username: "duplicate",
-      email: "duplicate",
-      last_name: "duplicate",
-      phone_number: "duplicate",
+  it("Tries to remove existing account insufficient information", async (done) => {
+    const response = await request.post("/api/account/fb/remove_account").send({
+      nonValid: "nonValid",
     });
     expect(response.status).toBe(400);
-    expect(response.body).toStrictEqual({ error: "Insufficient information" });
+    expect(response.body).toStrictEqual({ message: "UID not provided" });
+    done();
+  });
+  it("Tries to create account with invalid information (email)", async (done) => {
+    const response = await request.post("/api/account/fb/create_account").send({
+      email: "nonValid",
+      password: "test#@&(**Ud",
+    });
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({
+      code: "auth/invalid-email",
+      message: "The email address is improperly formatted.",
+    });
+    done();
+  });
+  it("Tries to create account with invalid information (bad password)", async (done) => {
+    const response = await request.post("/api/account/fb/create_account").send({
+      email: "badPassword@badPassword.com",
+      password: "short",
+    });
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({
+      code: "auth/invalid-password",
+      message: "The password must be a string with at least 6 characters.",
+    });
+    done();
+  });
+  it("Tries to remove existing account invalid information", async (done) => {
+    const response = await request.post("/api/account/fb/remove_account").send({
+      uid: "nonValid",
+    });
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({
+      code: "auth/user-not-found",
+      message:
+        "There is no user record corresponding to the provided identifier.",
+    });
+    done();
+  });
+  it("Tries to validate with insufficent information", async (done) => {
+    const response = await request.post("/api/account/fb/login").set({
+      nonValid: "nonValid",
+    });
+    // expect(response.status).toBe(401);
+    expect(response.body).toStrictEqual({ message: "No token provided" });
+    done();
+  });
+  it("Tries to validate with improper information (no bearer)", async (done) => {
+    const response = await request.post("/api/account/fb/login").set({
+      authorization: "nonValid",
+    });
+    // expect(response.status).toBe(401);
+    expect(response.body).toStrictEqual({ message: "Invalid token" });
+    done();
+  });
+  it("Tries to validate with invalid information", async (done) => {
+    const response = await request.post("/api/account/fb/login").set({
+      authorization: "Bearer nonValid",
+    });
+    expect(response.status).toBe(403);
+    expect(response.body).toStrictEqual({
+      code: "auth/argument-error",
+      message:
+        "Decoding Firebase ID token failed. Make sure you passed the entire string JWT which represents an ID token. See https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve an ID token.",
+    });
     done();
   });
 });
